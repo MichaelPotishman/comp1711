38ae34f7dbd7861b898117432f7ce6508f763811
MichaelPotishman <146711713+MichaelPotishman@users.noreply.github.com>
2023-10-16 09:27:00 +0000

    Adding

--- C_Bootcamp_week_2/arrayrotation.c
+++ C_Bootcamp_week_2/arrayrotation.c
@@ -4,16 +4,14 @@ int main(){
     int array[] = {2,4,3,5,7};
     int temp;
     for (int i = 0; i <= 4; i++){
+        temp = array[i];
         if (i == 4){
-            array[0] == array[4];
-        }else{
-            temp = array[i];
+            array[0] = temp;
+        } else {
             array[i + 1] == temp;
         }
-            
-            printf("%d ", &array);
         }
-
-
-        
+    for(int i=0; i<=4; i++) {
+        printf("%d ", array[i]); 
     } 
+}
