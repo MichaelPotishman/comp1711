7b486fed04b40c43c99905616166a4f6350f19ab
MichaelPotishman <146711713+MichaelPotishman@users.noreply.github.com>
2023-11-20 09:12:06 +0000

    adding everything i forgot to add

---
+++ C_Bootcamp_week_5/example.c
@@ -0,0 +1,13 @@
+typedef struct
+{
+    char date[20];
+    float bloodIron;
+} reading;
+
+//Typedef struct called 'reading'
+
+//Creating a single instance
+reading today;
+
+//Creating array with 7 members to store a weeks worth of readings
+reading thisWeek[7];
